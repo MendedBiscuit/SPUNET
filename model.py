feat: replaced DiceLoss with DiceLoss + FocalLoss fix: forgot normalisation in predict.py

model.py
@@ -26,6 +26,9 @@ class UNet(L.LightningModule):
         self.register_buffer("std", torch.tensor(params["std"]).view(1, 3, 1, 1))
         self.register_buffer("mean", torch.tensor(params["mean"]).view(1, 3, 1, 1))
 
+        self.dice_loss = smp.losses.DiceLoss(mode='binary', from_logits=True)
+        self.focal_loss = smp.losses.FocalLoss(mode='binary')
+
         self.loss_fn = smp.losses.DiceLoss(smp.losses.BINARY_MODE, from_logits=True)
 
         self.training_step_outputs = []
@@ -37,14 +40,19 @@ class UNet(L.LightningModule):
     def shared_step(self, batch, stage):
         image, mask = batch
 
+        if mask.ndim == 3:
+            mask = mask.unsqueeze(1)
+        
+        mask = (mask > 0).float()
         logits_mask = self.forward(image)
-        loss = self.loss_fn(logits_mask, mask)
+        loss = self.dice_loss(logits_mask, mask) + self.focal_loss(logits_mask, mask)
+        # loss = self.loss_fn(logits_mask, mask)
 
         prob_mask = logits_mask.sigmoid()
         pred_mask = (prob_mask > 0.5).float()
 
         tp, fp, fn, tn = smp.metrics.get_stats(
-            pred_mask.long(), mask.long().unsqueeze(1), mode="binary"
+            pred_mask.long(), mask.long(), mode="binary"
         )
 
         self.log(f"{stage}_loss", loss, prog_bar=True, on_step=True, on_epoch=True)

predict.py
@@ -1,14 +1,21 @@
 import torch
 import matplotlib.pyplot as plt
 import pytorch_lightning as L
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
 from torch.utils.data import DataLoader
 from dataset import SpunetDataset
 from model import UNet
 
-CHECKPOINT = "lightning_logs/version_2/checkpoints/model.ckpt"
+CHECKPOINT = "lightning_logs/version_4/checkpoints/model.ckpt"
 DATA = "./img/train/train_img"
 MASK = "./img/train/train_mask"
-NUM_PREDS = 10
+NUM_PREDS = 15
+PREDICT_TRANSFORM = A.Compose([
+    A.Resize(544, 544),
+    A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+    ToTensorV2(),
+])
 
 def predict_and_visualize():
     model = UNet.load_from_checkpoint(CHECKPOINT)
@@ -16,7 +23,7 @@ def predict_and_visualize():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
 
-    test_dataset = SpunetDataset(DATA, MASK)
+    test_dataset = SpunetDataset(DATA, MASK, transform=PREDICT_TRANSFORM)
     test_dataloader = DataLoader(test_dataset, batch_size=NUM_PREDS, shuffle=False)
 
     batch = next(iter(test_dataloader))
@@ -24,8 +31,8 @@ def predict_and_visualize():
 
     with torch.no_grad():
         logits = model(images.to(device))
-        pr_masks = (logits.sigmoid() > 0.5).float()
-        # pr_masks = logits.sigmoid()
+        # pr_masks = (logits.sigmoid() > 0.5).float()
+        pr_masks = logits.sigmoid()
 
     for idx in range(min(len(images), NUM_PREDS)):
         plt.figure(figsize=(15, 5))