fix: fixed bugged transforms and normalisation

predict.py
@@ -5,10 +5,10 @@ from torch.utils.data import DataLoader
 from dataset import SpunetDataset
 from model import UNet
 
-CHECKPOINT = "lightning_logs/version_2/checkpoints/epoch=199-step=200.ckpt"
+CHECKPOINT = "lightning_logs/version_2/checkpoints/model.ckpt"
 DATA = "./img/train/train_img"
 MASK = "./img/train/train_mask"
-
+NUM_PREDS = 10
 
 def predict_and_visualize():
     model = UNet.load_from_checkpoint(CHECKPOINT)
@@ -17,7 +17,7 @@ def predict_and_visualize():
     model.to(device)
 
     test_dataset = SpunetDataset(DATA, MASK)
-    test_dataloader = DataLoader(test_dataset, batch_size=7, shuffle=False)
+    test_dataloader = DataLoader(test_dataset, batch_size=NUM_PREDS, shuffle=False)
 
     batch = next(iter(test_dataloader))
     images, masks = batch
@@ -27,7 +27,7 @@ def predict_and_visualize():
         pr_masks = (logits.sigmoid() > 0.5).float()
         # pr_masks = logits.sigmoid()
 
-    for idx in range(min(len(images), 7)):
+    for idx in range(min(len(images), NUM_PREDS)):
         plt.figure(figsize=(15, 5))
 
         plt.subplot(1, 3, 1)